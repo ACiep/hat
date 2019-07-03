34020fbb251cc49ce706625ec931c0f4ccfcb8af
Just Me <ACiep@github.com>
2019-07-04 00:19:20 +0200

    pass headers to request

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ extern crate hyper;
 use clap::{App, Arg};
 use hyper::rt::{self, Future, Stream};
 use hyper::{Body, Client, Request, Response};
+use std::collections::HashMap;
 use std::io::{self, Write};
+
+const HEADER_PARSING_ERROR: &'static str = "Header must be specified as key: value";
 
 fn parse_body<T: Into<String>>(body: Option<T>) -> Body {
     match body {
@@ -18,6 +21,23 @@ fn parse_body<T: Into<String>>(body: Option<T>) -> Body {
             }
         }
     }
+}
+
+fn parse_headers(headers: clap::Values) -> HashMap<String, String> {
+    let mut ret: HashMap<String, String> = HashMap::new();
+    for header in headers {
+        let mut split = header.split(':');
+        ret.insert(
+            split
+                .clone()
+                .nth(0)
+                .expect(HEADER_PARSING_ERROR)
+                .trim()
+                .to_string(),
+            split.nth(1).expect(HEADER_PARSING_ERROR).trim().to_string(),
+        );
+    }
+    ret
 }
 
 fn handle_response<T>(res: Response<T>) {
@@ -51,6 +71,15 @@ fn main() {
                 .long("body")
                 .takes_value(true),
         )
+        .arg(
+            Arg::with_name("headers")
+                .help("Add HTTP header to request")
+                .required(false)
+                .short("h")
+                .long("header")
+                .multiple(true)
+                .takes_value(true),
+        )
         .get_matches();
 
     let url = matches.value_of("url").expect("Pass URL value");
@@ -60,10 +89,15 @@ fn main() {
         .to_string();
     let body = parse_body(matches.value_of("body"));
     let uri: hyper::Uri = url.parse().expect("URL is not correct");
+    let headers = parse_headers(matches.values_of("headers").unwrap());
 
     rt::run(rt::lazy(move || {
         let client = Client::new();
-        let req = Request::builder()
+        let mut req = Request::builder();
+        for (key, value) in headers {
+            req.header(key.as_str(), value.as_str());
+        }
+        let req = req
             .uri(uri)
             .method(method.as_str())
             .body(body)
